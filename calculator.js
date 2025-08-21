baed5334595ca4a6b28007e424380aaf88db0111
Celia Garcia <fgarciacelia@gmail.com>
2025-08-21 17:55:57 +0200

    Operacion basica con dos numeros

--- calculator.js
+++ calculator.js
@@ -33,9 +33,44 @@ function operate(operator, number1, number2){
 let output = document.querySelector(".output");
 
 function clearOutput() {
-    output.textContent = "";
+    output.textContent = "0";
     outputValue = "";
 }
 
  let clear = document.querySelector(".clear");
 clear.addEventListener("click", clearOutput)
+
+
+let numbers = document.querySelectorAll(".number");
+let outputValue = "";
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        outputValue += number.textContent;
+        output.textContent = outputValue;
+    })
+})
+
+
+let number1;
+let number2;
+let operateWith;
+
+
+let operators = document.querySelectorAll(".operator");
+operators.forEach(op => {
+    op.addEventListener("click", () => {
+        number1 = parseFloat(outputValue);
+        operateWith = op.textContent;
+        outputValue = "";
+        output.textContent = "";
+    })
+})
+
+let result = document.querySelector(".equal");
+result.addEventListener("click", () => {
+    number2 = parseFloat(outputValue);
+    let finalResult = operate(operateWith, number1, number2);
+    output.textContent = finalResult;
+    outputValue = finalResult; 
+})
+
