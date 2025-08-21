afcf05d3df12da3758bb04f14144cfb59f5a103b
Celia Garcia <fgarciacelia@gmail.com>
2025-08-21 11:57:24 +0200

    Operate basic function

---
+++ calculator.js
@@ -0,0 +1,31 @@
+function add(number1, number2){
+    return number1 + number2;
+}
+
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+function divide(number1, number2){
+    return number1/number2;
+}
+
+function operate(operator, number1, number2){
+    if (operator === "+"){
+        return add(number1, number2);
+    } else if (operator === "-") {
+        return subtract(number1, number2);
+    } else if (operator === "*" || operator === "x") {
+        return multiply(number1, number2);
+    } else {
+        if(number2 == 0){
+            return "No se puede dividir entre 0."
+        } else {
+            return divide(number1, number2);
+        }
+    }
+}
