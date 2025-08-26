d56ec6b977dd3e6be27a1815c5a5ea8e7881225c
Celia Garcia <fgarciacelia@gmail.com>
2025-08-26 21:04:11 +0200

    Evento click en =

--- calculator.js
+++ calculator.js
@@ -14,23 +14,29 @@ function divide(number1, number2){
     return number1/number2;
 }
 
+function roundNumber(result){
+    return Math.round(result * 1000) / 1000;
+}
+
 function operate(operator, number1, number2){
     if (operator === "+"){
-        return add(number1, number2);
+        return roundNumber(add(number1, number2));
     } else if (operator === "-") {
-        return subtract(number1, number2);
+        return roundNumber(subtract(number1, number2));
     } else if (operator === "*" || operator === "x") {
-        return multiply(number1, number2);
+        return roundNumber(multiply(number1, number2));
     } else {
         if(number2 == 0){
             return "No se puede dividir entre 0."
         } else {
-            return divide(number1, number2);
+            return roundNumber(divide(number1, number2));
         }
     }
 }
 
 let output = document.querySelector(".output");
+
+let outputValue = "";
 
 function clearOutput() {
     output.textContent = "0";
@@ -45,7 +51,6 @@ clear.addEventListener("click", clearOutput)
 
 
 let numbers = document.querySelectorAll(".number");
-let outputValue = "";
 numbers.forEach(number => {
     number.addEventListener("click", () => {
         outputValue += number.textContent;
@@ -62,25 +67,27 @@ let operateWith;
 let operators = document.querySelectorAll(".operator");
 operators.forEach(op => {
     op.addEventListener("click", () => {
-if (number1 !== undefined && operateWith !== undefined){
+if (number1 !== undefined && operateWith && outputValue !== ""){
     number2= parseFloat(outputValue);
     let currentResult = operate(operateWith,number1,number2);
     output.textContent = currentResult;
-    outputValue = currentResult;
+    number1 = currentResult;
+    outputValue = "";
 } else {
         number1 = parseFloat(outputValue);
-        operateWith = op.textContent;
         outputValue = "";
-        output.textContent = "";
     }    
+    operateWith = op.textContent;
 });
 });
 
 let result = document.querySelector(".equal");
 result.addEventListener("click", () => {
-    number2 = parseFloat(outputValue);
+    number2 = parseFloat(outputValue)
     let finalResult = operate(operateWith, number1, number2);
     output.textContent = finalResult;
     outputValue = finalResult; 
+    number1 = finalResult;
 })
+
 
